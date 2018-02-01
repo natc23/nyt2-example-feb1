9357ffe58f8f8f1fe37357c66e128098f192fa42
Natalie <nataliecieslak@yahoo.com>
2018-02-01 10:04:27 -0500

    added first files

---
+++ nyt.py
@@ -0,0 +1,14 @@
+import requests
+import Secrets
+
+
+# gets stories from a particular section of NY times
+def get_stories(section):
+    baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
+    extendedurl = baseurl + section + '.json'
+    params={'api-key': Secrets.nyt_key}
+    return requests.get(extendedurl, params).json()
+
+section = 'science'
+stories = get_stories(section)
+print(stories) # should print a big pile of json
